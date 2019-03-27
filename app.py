f70100a5abd5a929584c42bfe203a934286ccf77
CamCam <courage@camcam.cam>
2019-03-27 16:41:27 -0700

    General updates and added ToDo for status page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, make_response
+from flask import Flask, send_file, make_response
 from datetime import datetime
 import os
 from collections import defaultdict
@@ -13,8 +13,8 @@ check_in_dict = defaultdict(list)
 @app.route('/<kiosk>', methods=['POST'])
 def check_in(kiosk):
     check_in_dict[kiosk].insert(0, datetime.now())
-    print(check_in_dict)
     return make_response(kiosk + " has checked in successfully.", 200)
+
 
 @app.route('/getlog', methods=['GET'])
 def get_log():
@@ -27,16 +27,29 @@ def get_log():
                     now_min = sum([datetime.now().time().hour * 60, datetime.now().time().minute])
                     k_min = sum([check_in_dict[kiosk][0].time().hour * 60, check_in_dict[kiosk][0].time().minute])
                     if now_min - k_min > 240:
+                        check_in_dict[kiosk].insert(0, False)
                         file.writelines("WARNING: " + kiosk + " has not recently checked in!!\n\n")
                 else:
+                    check_in_dict[kiosk].insert(0, False)
                     file.writelines("WARNING: " + kiosk + " has not recently checked in!!\n")
                 for checkin in check_in_dict[kiosk]:
+                    if checkin is False:
+                        continue
                     file.writelines("Checked in at: " + checkin.strftime("%Y/%m/%d %H:%M") + "\n")
                 file.writelines("\n\n")
         return send_file(str(os.path.join(root_dir, 'log.txt')), attachment_filename='log.txt')
     except Exception as e:
         return str(e), 400
 
+
+@app.route('/clear', methods=['POST'])
+def clear():
+    check_in_dict.clear()
+
+# TODO: Write a method to return an html page for status of kiosks
+@app.route('/online', methods=['GET'])
+def online():
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
